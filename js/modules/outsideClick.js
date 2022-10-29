7e2eb2756d532dd7639520ad064b3272a29c6329
Lucas <lucasfbnavarro@gmail.com>
2022-10-29 19:57:37 -0300

    correcao de bugs no menu mobile
    correção de bug onde mudei a função outsideClick para fechar quando o usuário clica no próprio botão que foi clicado e aumentei a área de contato dos botões no menu mobile

--- js/modules/outsideClick.js
+++ js/modules/outsideClick.js
@@ -6,7 +6,12 @@ export default function outsideClick(element, callback) {
     setTimeout(() => html.addEventListener("click", htmlClick));
   }
   function htmlClick(event) {
-    if (!element.contains(event.target)) {
+    if (
+      !element.contains(event.target) ||
+      !element.contains(event.target.dataset.dropdown) ||
+      element.contains(element)
+    ) {
+      console.log(element.dataset.dropdown);
       element.removeAttribute(outside);
       html.removeEventListener("click", htmlClick);
       callback();
